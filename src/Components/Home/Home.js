ff12a5ac5cc58aeb77a5b74718813d285352e946
Alex Buchanan <alex.n.buchanan@gmail.com>
2022-07-14 21:49:40 -0700

    added new images

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,63 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Home.css";
+import { gsap } from "gsap";
+import * as THREE from "three";
 
 export default function Home() {
+  // const scene = new THREE.Scene();
+
+  useEffect(() => {
+    const items1 = document.querySelectorAll(".item1");
+    const items2 = document.querySelectorAll(".item2");
+
+    items1.forEach((el) => {
+      const image = el.querySelector("img");
+
+      el.addEventListener("mouseenter", (e) => {
+        gsap.to(image, { autoAlpha: 1 });
+      });
+
+      el.addEventListener("mouseleave", (e) => {
+        gsap.to(image, { autoAlpha: 0 });
+      });
+
+      el.addEventListener("mousemove", (e) => {
+        gsap.set(image, { x: e.offsetX - 200 });
+      });
+    });
+
+    items2.forEach((el) => {
+      const image = el.querySelector("img");
+
+      el.addEventListener("mouseenter", (e) => {
+        gsap.to(image, { autoAlpha: 1 });
+      });
+
+      el.addEventListener("mouseleave", (e) => {
+        gsap.to(image, { autoAlpha: 0 });
+      });
+
+      el.addEventListener("mousemove", (e) => {
+        gsap.set(image, { x: e.offsetX - -500 });
+      });
+    });
+  }, []);
+
   return (
     <body>
-      <h2>Alex Buchanan is a web developer based in Los Angeles, CA</h2>
+      <div className="mainText">
+        <div className="item1" style={{ display: "inline" }}>
+          <img src="/images/zlatan.jpg" />
+          <u style={{ textDecorationThickness: "1px" }}>Alex Buchanan</u>
+        </div>{" "}
+        is a web developer based in <br />
+        <div className="item2" style={{ display: "inline" }}>
+          <img className="tiger" src="/images/la_city_hall.jpg" />
+          <div style={{ float: "right" }}>
+            <u style={{ textDecorationThickness: "1px" }}>Los Angeles, CA</u>
+          </div>
+        </div>
+      </div>
     </body>
   );
 }
--- src/Components/NavBar/Nav.js
+++ src/Components/NavBar/Nav.js
@@ -6,16 +6,10 @@ export default function Nav() {
     <div>
       <ul>
         <li>
-          <a href="#">Home</a>
-        </li>
-        <li>
-          <a href="#">News</a>
-        </li>
-        <li>
-          <a href="#">Contact</a>
-        </li>
-        <li>
           <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Projects</a>
         </li>
       </ul>
     </div>
