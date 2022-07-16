3391982a5f7efe027d11435035cad27909b463da
Alex Buchanan <alex.n.buchanan@gmail.com>
2022-07-16 14:12:31 -0700

    added background grain

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,15 +3,31 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home/Home";
 import About from "./About";
 import Nav from "./NavBar/Nav";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    /*const options = {
+      animate: true,
+      patternWidth: 100,
+      patternHeight: 100,
+      grainOpacity: 0.05,
+      grainDensity: 1,
+      grainWidth: 1,
+      grainHeight: 1,
+    };*/
+    window.grained("#divContainer", {});
+  }, []);
+
   return (
     <Router>
       <Nav />
-      <Routes>
-        <Route path="/about" element={<About />} />
-        <Route path="/" exact element={<Home />} />
-      </Routes>
+      <div id="divContainer">
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/" exact element={<Home />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,11 +1,8 @@
 import React, { useState, useEffect } from "react";
 import "./Home.css";
 import { gsap } from "gsap";
-import * as THREE from "three";
 
 export default function Home() {
-  // const scene = new THREE.Scene();
-
   useEffect(() => {
     const items1 = document.querySelectorAll(".item1");
     const items2 = document.querySelectorAll(".item2");
@@ -43,21 +40,25 @@ export default function Home() {
     });
   }, []);
 
+  grained("#container", {});
+
   return (
-    <body>
-      <div className="mainText">
-        <div className="item1" style={{ display: "inline" }}>
-          <img src="/images/zlatan.jpg" />
-          <u style={{ textDecorationThickness: "1px" }}>Alex Buchanan</u>
-        </div>{" "}
-        is a web developer based in <br />
-        <div className="item2" style={{ display: "inline" }}>
-          <img className="tiger" src="/images/la_city_hall.jpg" />
-          <div style={{ float: "right" }}>
-            <u style={{ textDecorationThickness: "1px" }}>Los Angeles, CA</u>
+    <div id="container" style={{ height: "calc(100vh - 50px)" }}>
+      <div class="contents" style={{ height: "100%" }}>
+        <div className="mainText">
+          <div className="item1" style={{ display: "inline" }}>
+            <img src="/images/zlatan.jpg" />
+            <u style={{ textDecorationThickness: "1px" }}>Alex Buchanan</u>
+          </div>{" "}
+          is a web developer based in <br />
+          <div className="item2" style={{ display: "inline" }}>
+            <img className="tiger" src="/images/la_city_hall.jpg" />
+            <div style={{ float: "right" }}>
+              <u style={{ textDecorationThickness: "1px" }}>Los Angeles, CA</u>
+            </div>
           </div>
         </div>
       </div>
-    </body>
+    </div>
   );
 }
